80c1641821d2271b0f19e864f95ba8759a119ce2
Roman Panshin <roma.super@icloud.com>
2021-01-24 20:50:39 +0300

    add Swagger API documentation

--- Group/src/main/java/com/Xjournal/Group/Configurator/MvcConfig.java
+++ Group/src/main/java/com/Xjournal/Group/Configurator/MvcConfig.java
@@ -1,12 +1,20 @@
 package com.Xjournal.Group.Configurator;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
 @EnableWebMvc
+@EnableSwagger2WebMvc
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -14,4 +22,5 @@ public class MvcConfig implements WebMvcConfigurer {
                 .addResourceHandler("/r/**")
                 .addResourceLocations("file:/Volumes/podarochek/1/Storage/");
     }
+
 }
--- Group/src/main/java/com/Xjournal/Group/Controller/RootController.java
+++ Group/src/main/java/com/Xjournal/Group/Controller/RootController.java
@@ -1,13 +1,13 @@
-package com.Xjournal.Group.Controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-@Controller
-@RequestMapping("/")
-public class RootController {
-    @RequestMapping(method = RequestMethod.GET)
-    public String swaggerUi() {
-        return "redirect:/swagger-ui.html";
-    }
-}
+//package com.Xjournal.Group.Controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//@Controller
+//@RequestMapping("/")
+//public class RootController {
+//    @RequestMapping(method = RequestMethod.GET)
+//    public String swaggerUi() {
+//        return "redirect:/swagger-ui.html";
+//    }
+//}
