9adb98c32d3529ada863168f8163d68b73327eec
Roman Panshin <roma.super@icloud.com>
2021-02-03 23:56:55 +0300

    FileSize2

--- Group/src/main/java/com/Xjournal/Group/GroupApplication.java
+++ Group/src/main/java/com/Xjournal/Group/GroupApplication.java
@@ -3,6 +3,11 @@ package com.Xjournal.Group;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
 
 
 @SpringBootApplication
@@ -10,5 +15,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GroupApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GroupApplication.class, args);
+	}
+
+	@Bean
+	MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setMaxFileSize(DataSize.ofBytes(512000000L));
+		factory.setMaxRequestSize(DataSize.ofBytes(512000000L));
+		return factory.createMultipartConfig();
 	}
 }
